54ad25ec9230a4d57fe574d09bb71df820816859
NfSparrow <smpxneeli@gmail.com>
2026-01-15 18:47:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mn-bots/shobanafilterbot.git /shobanafilterbot
+  git clone https://github.com/NfSparrow/NEELI_XBOT.git /shobanafilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /shobanafilterbot
+  git clone $UPSTREAM_REPO /NEELI_XBOT
 fi
-cd /shobanafilterbot
+cd /NEELI_XBOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
